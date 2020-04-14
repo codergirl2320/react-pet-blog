6a015fbf67c497fcfbe8470a1f0476ae46cb1cf7
srl23 <stephen.linfield@comcast.net>
2020-04-14 18:59:47 -0400

    first changes made, will make more for some time

--- pet_blog/src/App.js
+++ pet_blog/src/App.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>potato</h1>
-    </div>
-  );
+//the components go here
+
+
+//the app class
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+        view: {
+          pageName: 'home',
+          pageTitle: ''
+        },
+        formInputs: {
+          name: null,
+          title: null,
+          post: null,
+          avatar: null, //the little pictures one sees on blog or forum posts for a user's picture
+          id: null
+        }
+     }
+  }
+
+  viewHandler = (view, blogData) => {
+    let pageTitle = '';
+    formInputs = {
+      name: '',
+      title: '',
+      post: '',
+      avatar: '',
+      id: null
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Welcome to the Secret Lives of Pets, a blog for pets by pets!</h1>
+      </div>
+    )
+  }
 }
 
 
