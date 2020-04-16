f0144d2ebb834d2b5c89ab79ba69d947e8f3bdd4
srl23 <stephen.linfield@comcast.net>
2020-04-15 21:00:12 -0400

    more work on post.js

--- pet_blog/src/components/Post.js
+++ pet_blog/src/components/Post.js
@@ -4,7 +4,20 @@ class Post extends React.Component {
   render() {
     return (
       <article>
-        <div>
+        <div className="post-header">
+          <img src={this.props.postData.avatar} alt=""/>
+          <h1>{this.props.postData.name} barked: </h1>
+        </div>
+        <div className="post-body">
+          {this.props.postData.post}
+        </div>
+        <div className="post-options">
+          <button onClick={() => {
+            this.props.viewHandler('editPost', this.props.postData)
+          }}>Edit Entry</li>
+          <button onClick={() => {
+            this.props.deleteHandler(this.props.postData.id)
+          }}>Delete Entry</li>
         </div>
       </article>
     )
