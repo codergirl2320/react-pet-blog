7672962e4bc95d4195974fd7ad640f868334eb60
Reshaeala <58646198+Reshaeala@users.noreply.github.com>
2020-04-17 16:28:05 -0500

    adding some info into like and about

---
+++ pet_blog/src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class About extends React.Component {
+  render(){
+    return (
+      <div>
+      About
+      </div>
+    )
+
+  }
+
+}
+
+export default About
---
+++ pet_blog/src/components/Like.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Like extends React.Component {
+  render(){
+    return (
+      <div>
+      Likes button here
+      </div>
+    )
+
+  }
+
+}
+
+export default About
