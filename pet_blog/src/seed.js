d3fa3a6d8cc70147b604994056231e41e447867a
Tania Diane Dodson <taniad0916@MacBook.local>
2020-04-17 19:53:36 -0400

    changed seed data

--- pet_blog/src/seed.js
+++ pet_blog/src/seed.js
@@ -2,17 +2,17 @@ NORMAN
 
 GOTTA LOVE HAMSTER LIFE...
 
-COVID-19 IS WORKING OUT GREAT FOR ME! ALL MY HUMANS ARE HOME AND ENTERTAINING ME AROUND THE CLOCK...I GO FROM A/C VENT TO A/C VENT WATCHING THEIR ANTICS...THERE'S COOKING IN THE KITCHEN...A TALE OF KNIGHTS IN THE KID'S ROOM...HILARIOUS MAN CONVERSATION IN THE LIVING ROOM...REALLY HOPE IT NEVER ENDS!
+COVID-19 is working out great for ME! All my humans are home and entertaining me around the clock...I go from a/c vent to a/c vent watching their antics...there's cooking in the kitchen...a tale of knights in the kid's room...and hilarious man conversation in the living room...REALLY hope it never ends!
 
-/IMAGES/NORMAN_AVATAR.PNG
+/images/Norman_avatar.png
 
 GIDGET
 
 SEEING MY TRUE LOVE MAX LATER...
 
-SO I'VE TREATED MYSELF TO A SPA DAY...GOT EXTRA FLUFFY IN AN EARLY MORNING WASH CYCLE IN THE DISHWASHER...RESTED WITH CUCUMBER SLICES ON MY PUFFY EYES...AND CHEWED A FEW MILK BONES TO CLEAN MY TEETH...
+So i've treated myself to a spa day...got extra fluffy in an early morning wash cycle in the dishwasher...rested with cucumber slices on my puffy eyes...and chewed a few milk bones to clean my teeth..
 
-/IMAGES/GIDGET_AVATAR.JPG
+/images/Gidget_avatar.jpg
 
 Sweetpea
 
