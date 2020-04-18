fb036e313fcb21422725d831ff32ed722b1f8876
Tania Diane Dodson <taniad0916@MacBook.local>
2020-04-18 02:29:47 -0400

    even more styling

--- pet_blog/src/components/Form.js
+++ pet_blog/src/components/Form.js
@@ -56,7 +56,7 @@ class Form extends React.Component {
         </label>
 
         <input type="submit" value="share"/><br/>
-        <img className="Ozone" src="./images/Ozone.png" alt=""/>
+        
         <img className="Sweetpea" src="./images/Sweetpea.png" alt=""/>
       </form>
     )
